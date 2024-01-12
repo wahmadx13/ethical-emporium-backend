7226b40e95177acd7161c261a70d435cdcf0e235
Waseem <wasim_ahmad2003@yahoo.com>
2024-01-12 18:18:02 +0500

    configured app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 // imports
-
 import express from "express";
+import database from "./config/database";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import notFound from "./middleware/notFound";
+import errorHandler from "./middleware/errorHandler";
+import dotenv from "dotenv";
 
 //initialize express
 const app = express();
 
+//middlewares
+dotenv.config();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(cors());
+
+//connect DB
+database();
+
 //Port
 const PORT = process.env.PORT || 4000;
+
+//error handlers
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
