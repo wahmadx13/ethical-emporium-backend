0459ec8336a9d4bb2f432be85274be6db584f52a
Waseem <wasim_ahmad2003@yahoo.com>
2024-01-31 14:43:42 +0500

    uninstall unawanted dependencies

--- dist/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFound = (request, response, next) => {
-    const error = new Error(`Not Found: ${request.originalUrl}`);
-    response.status(404);
-    next(error);
-};
-exports.default = notFound;
-//# sourceMappingURL=notFound.js.map
