49f45202784633148095ff5fa8dfedd4bd5490aa
Waseem <wasim_ahmad2003@yahoo.com>
2024-02-28 23:11:07 +0500

    modify product category responses for better understanding

--- src/services/productCategoryServices/index.ts
+++ src/services/productCategoryServices/index.ts
@@ -6,27 +6,71 @@ import { ProductCategory } from "../../models/productCategory";
 import { ProductCategoryModel } from "../../models";
 
 //Create A Product Category
-const createProductCategory = expressAsyncHandler(
-  async (request: Request, response: Response): Promise<void> => {
+const createProductCategory = async (
+  request: Request,
+  response: Response
+): Promise<void> => {
+  try {
+    const findProductCategory: DocumentType<ProductCategory> | null =
+      await ProductCategoryModel.findOne(request.body);
+    if (findProductCategory) {
+      response.json({
+        statusCode: 304,
+        message: `Product category: ${request.body.title} already exists. Please try adding a new one.`,
+      });
+      return;
+    }
+
     const createCategory: DocumentType<ProductCategory> =
       await ProductCategoryModel.create(request.body);
 
-    response.json(createCategory);
+    response.json({
+      statusCode: 200,
+      message: `Product category: ${request.body.title} added successfully!`,
+      createCategory,
+    });
+  } catch (err) {
+    response.json({
+      statusCode: 500,
+      message: err,
+    });
   }
-);
+};
 
 //Update A Product Category
-const updateProductCategory = expressAsyncHandler(
-  async (request: Request, response: Response): Promise<void> => {
-    const { id } = request.params;
-    validateMongoDBId(id);
-    const updateCategory: DocumentType<ProductCategory> | null =
+const updateProductCategory = async (
+  request: Request,
+  response: Response
+): Promise<void> => {
+  const { id } = request.params;
+  validateMongoDBId(id);
+  try {
+    console.log("updating product category");
+    const findProductCategory: DocumentType<ProductCategory> | null =
+      await ProductCategoryModel.findById(id);
+    if (findProductCategory?.title === request.body?.title) {
+      response.json({
+        statusCode: 304,
+        message: `Product category: ${findProductCategory?.title} already exists. Please try adding a new one`,
+      });
+      return;
+    }
+    const updateProductCategory: DocumentType<ProductCategory> | null =
       await ProductCategoryModel.findByIdAndUpdate(id, request.body, {
         new: true,
       });
-    response.json(updateCategory);
+    response.json({
+      statusCode: 200,
+      message: "Product category updated successfully!",
+      updateProductCategory,
+    });
+  } catch (err) {
+    response.json({
+      statusCode: 500,
+      message: err,
+    });
   }
-);
+};
 
 //Get A Product Category
 const getAProductCategory = expressAsyncHandler(
@@ -49,15 +93,27 @@ const getAllProductCategories = expressAsyncHandler(
 );
 
 //Delete A Product Category
-const deleteAProductCategory = expressAsyncHandler(
-  async (request: Request, response: Response): Promise<void> => {
-    const { id } = request.params;
-    validateMongoDBId(id);
+const deleteAProductCategory = async (
+  request: Request,
+  response: Response
+): Promise<void> => {
+  const { id } = request.params;
+  validateMongoDBId(id);
+  try {
     const deleteProductCategory: DocumentType<ProductCategory> | null =
       await ProductCategoryModel.findByIdAndDelete(id);
-    response.json(deleteProductCategory);
+    response.json({
+      statusCode: 200,
+      message: `Product category: ${deleteProductCategory?.title} deletion successful`,
+      deleteProductCategory,
+    });
+  } catch (err) {
+    response.json({
+      statusCode: 500,
+      message: err,
+    });
   }
-);
+};
 
 export {
   createProductCategory,
